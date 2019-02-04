0b52498a1cea3adab072726db605778c55e7a5fa
Dylan Landon <dylan43landon@gmail.com>
2019-02-04 13:02:29 -0800

    Assignment 2 complete

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const marcoBase = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
 
@@ -20,4 +21,26 @@ describe("routes : static", () => {
     });
 
   });
+
+  describe("GET /marco", () => {
+    it("should return a status code 200", (done) => {
+      request.get(marcoBase, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+    });
+
+  it("should return string 'Polo'", (done) => {
+    request.get(marcoBase, (err,res, body) =>{
+      expect(body).toContain('Polo');
+      done();
+      });
+    });
+  });
 });
+
+// For this assignment, TDD a route that accepts a
+// GET request to /marco. The test should confirm that
+// a status code 200 is returned along with the
+// expectation that the body of the response should
+// contain the string "polo".
