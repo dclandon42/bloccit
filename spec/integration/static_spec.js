08c38ac8adc1686510bb3768284dd48ec23b0945
Dylan Landon <dylan43landon@gmail.com>
2019-02-05 11:34:33 -0800

    checkpoint bloccit 5 topics p1: complete

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -3,30 +3,13 @@ const server = require("../../src/server");
 const base = "http://localhost:3000/";
 
 describe("routes : static", () => {
-
-//#1
   describe("GET /", () => {
-
-//#2
-    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
-
-//#3
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(body).toContain("Welcome to Bloccit");
-
-//#4
         done();
       });
     });
-
   });
-
-  describe("GET /about", () => {
-   it("should return status code 200 and string 'About Us' in the body of the response", (done) => {
-     request.get(base, (err, res, body) => {
-       expect(res.statusCode).toBe(200);
-        expect(body).toContain("About Us");
-        done();
-      });
-    });
+});
